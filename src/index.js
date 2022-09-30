0546bea9e3fbcc6f52a9fbb9d44e6da1692536fd
vitor-carmo <vitorv0071@gmail.com>
2022-09-30 17:26:15 -0300

    better props

--- src/index.js
+++ src/index.js
@@ -5,40 +5,32 @@ export default function CookiesPolicy({
   styles = {
     containerBackgroundColor: null,
     linkColor: null,
-    button: {
-      backgroundColor: null,
-      textColor: null,
-      fontFamily: "arial"
-    },
-    paragraph: {
-      fontFamily: "arial",
-      color: null
-    },
+    buttonBackgroundColor,
+    buttonTextColor,
+    buttonFontFamily,
+    paragraphFontFamily,
+    paragraphColor,
   },
-  cookiesOptions = {
-    cookiesPolicyLink: {
-      href: "#",
-      target: "_blank",
-      rel: "noopener noreferrer",
-    },
-    cookieLocalStorageKey: "COOKIES_CONCENT",
-    cookieLocalStorageValue: "yes",
-  },
-  onAcceptCookies = () => {}
+  cookiesPolicyLinkHref = "#",
+  cookiesPolicyLinkTarget = "_blank",
+  cookiesPolicyLinkRel = "noopener noreferrer",
+  cookiesLocalStorageKey = "COOKIES_CONCENT",
+  cookiesLocalStorageValue = "yes",
+  onAcceptCookies = () => {},
 }) {
   const [showCookieContainer, setShowCookieContainer] = useState(false);
 
   const handleOnAcceptCookies = () => {
     window.localStorage.setItem(
-      cookiesOptions.cookieLocalStorageKey,
-      cookiesOptions.cookieLocalStorageValue
+      cookiesLocalStorageKey,
+      cookiesLocalStorageValue
     );
     setShowCookieContainer(false);
     onAcceptCookies();
   };
 
   useEffect(() => {
-    if (!window.localStorage.getItem(cookiesOptions.cookieLocalStorageKey)) {
+    if (!window.localStorage.getItem(cookiesLocalStorageKey)) {
       setShowCookieContainer(true);
     }
   }, []);
@@ -49,11 +41,11 @@ export default function CookiesPolicy({
           id="cookies-container"
           $containerBackgroundColor={styles?.containerBackgroundColor}
           $linkColor={styles?.linkColor}
-          $buttonBackgroundColor={styles?.button.backgroundColor}
-          $buttonTextColor={styles?.button.textColor}
-          $buttonFontFamily={styles?.button.fontFamily}
-          $paragraphColor={styles?.paragraph.color}
-          $paragraphFontFamily={styles?.paragraph.fontFamily}
+          $buttonBackgroundColor={styles?.buttonBackgroundColor}
+          $buttonTextColor={styles?.buttonTextColor}
+          $buttonFontFamily={styles?.buttonFontFamily}
+          $paragraphColor={styles?.paragraphColor}
+          $paragraphFontFamily={styles?.paragraphFontFamily}
         >
           <div className="cookie-container">
             <p>
@@ -63,9 +55,9 @@ export default function CookiesPolicy({
               você concorda com tal monitoramento. Para saber mais sobre os
               cookies que usamos, consulte nossa{" "}
               <a
-                href={cookiesOptions.cookiesPolicyLink.href}
-                target={cookiesOptions.cookiesPolicyLink.target}
-                rel={cookiesOptions.cookiesPolicyLink.rel}
+                href={cookiesPolicyLinkHref}
+                target={cookiesPolicyLinkTarget}
+                rel={cookiesPolicyLinkRel}
               >
                 política de cookies.
               </a>
